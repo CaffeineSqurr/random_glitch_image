e5b61446c531b0f20616363c6c99165c9039d0dc
Elliot Branson <ebranson@cs.umn.edu>
2015-10-26 15:29:22 -0700

    Creating setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='vitium',
+    version='0.1',
+    author = 'Elliot Branson',
+    py_modules=['vitium'],
+    packages=['vitium'],
+    install_requires=[
+        'jpglitch',
+        'Pillow',
+    ],
+    long_description=read('README'),
+    entry_points='''
+        [console_scripts]
+        vitium=vitium:cli
+    ''',
+)
